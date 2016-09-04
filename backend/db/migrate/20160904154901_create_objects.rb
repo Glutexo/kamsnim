a69ddd398099deccb8d9bc0bb9a2029af4e10e0b
Glutexo <glutexo@icloud.com>
2016-09-04 18:24:11 +0200

    Create first MC pair for items.
    Table objects renamed to items because there is an internal Ruby class named Object. Generated Item model and Items controller. Added items resources route. Changed ApplicationController from Base to API. Added items#index action retreiving all items as JSON.

--- backend/db/migrate/20160904154901_create_objects.rb
+++
@@ -1,7 +0,0 @@
-class CreateObjects < ActiveRecord::Migration[5.0]
-  def change
-    create_table :objects do |t|
-      t.string :title
-    end
-  end
-end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160904154901) do
+ActiveRecord::Schema.define(version: 20160904160351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "objects", force: :cascade do |t|
-    t.string "title"
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
